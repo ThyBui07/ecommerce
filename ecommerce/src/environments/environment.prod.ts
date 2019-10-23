1a81496e3285b66c8bf05b161c690d2796cdf27a
ThyBui07 <48641093+ThyBui07@users.noreply.github.com>
2019-10-23 10:08:25 +0300

    add home button

--- ecommerce/src/environments/environment.prod.ts
+++ ecommerce/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQTfCXs6olMqoIU8Fz36IMl8EskiOLNfM",
+    authDomain: "ecommerce-project-20845.firebaseapp.com",
+    databaseURL: "https://ecommerce-project-20845.firebaseio.com",
+    projectId: "ecommerce-project-20845",
+    storageBucket: "ecommerce-project-20845.appspot.com",
+    messagingSenderId: "465252557933",
+    appId: "1:465252557933:web:298d0665e7988715d5cb64"
+  }
 };
